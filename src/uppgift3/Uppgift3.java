7b5bf1115615b7398b304809835f76ed4fbb728e
isami <isamimitani@live.se>
2018-10-24 13:49:32 +0200

    init commit

---
+++ src/uppgift3/Uppgift3.java
@@ -0,0 +1,17 @@
+
+package uppgift3;
+
+/**
+ *
+ * @author isami
+ */
+public class Uppgift3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+
+}
