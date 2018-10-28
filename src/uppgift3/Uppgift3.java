0727bd5a312ecb1d697d6296faf3c033e87232ee
Julia Gu <Juejing.Gu@yh.nackademin.se>
2018-10-28 22:35:41 +0100

    BildSpel fr Julia

--- src/uppgift3/Uppgift3.java
+++ src/uppgift3/Uppgift3.java
@@ -1,17 +1,47 @@
 
 package uppgift3;
+import javax.swing.*;
 
 /**
  *
- * @author isami
+ * blanda first or you win first?
+ * hur kan funkar for bild
+ * Sen ska försöka använda CSS
+ * @author julia
  */
 public class Uppgift3 {
- 
-    /**
-     * @param args the command line arguments  
-     */
-    public static void main(String[] args) {
+    public Uppgift3(){
+        boolean again = true;
+        int row = 0;
+        String input = JOptionPane.showInputDialog("How many rows?(2-10)");
+        while(again){
+            try{
+                if(input==null || input.trim().equals(""))
+                    System.exit(0);
+                row = Integer.parseInt(input);
+                if(row>=2 && row<=10)
+                    again = false;
+                else{
+                    input = JOptionPane.showInputDialog("Write number 2 to 10 !");
+                    again = true;
+                }
+                    
+            }
+            catch(Exception exc){
+                input = JOptionPane.showInputDialog("Write number 2 to 10 !");
+                again = true;
+            }
+        }
+        SpelCentral sc = new SpelCentral(row);
+    }
         
+
+        
+
+    
+    public static void main(String[] args) {
+//        BildSpel bs = new BildSpel();
+        Uppgift3 play = new Uppgift3();
     }
 
 }
