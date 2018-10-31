726762e9c53ebc56f7d7a2713ae2459cf5358aac
Julia Gu <Juejing.Gu@yh.nackademin.se>
2018-10-31 13:43:30 +0100

    new commit

--- src/uppgift3/SpelCentral.java
+++ src/uppgift3/SpelCentral.java
@@ -1,4 +1,4 @@
-/*
+ /*
  * Java
  */
 package uppgift3;
--- src/uppgift3/Uppgift3.java
+++ src/uppgift3/Uppgift3.java
@@ -10,32 +10,32 @@ import javax.swing.*;
  * @author julia
  */
 public class Uppgift3 {
-//    public Uppgift3(){
-//        boolean again = true;
-//        int row = 0;
-//        String input = JOptionPane.showInputDialog("How many rows?(2-10)");
-//        while(again){
-//            try{
-//                if(input==null || input.trim().equals(""))
-//                    System.exit(0);
-//                row = Integer.parseInt(input);
-//                if(row>=2 && row<=10)
-//                    again = false;
-//                else{
-//                    input = JOptionPane.showInputDialog("Write number 2 to 10 !");
-//                    again = true;
-//                }
-//                    
-//            }
-//            catch(Exception exc){
-//                input = JOptionPane.showInputDialog("Write number 2 to 10 !");
-//                again = true;
-//            }
-//        }
-//        SpelCentral sc = new SpelCentral(row);
-//    } 
+    public Uppgift3(){
+        boolean again = true;
+        int row = 0;
+        String input = JOptionPane.showInputDialog("How many rows?(2-10)");
+        while(again){
+            try{
+                if(input==null || input.trim().equals(""))
+                    System.exit(0);
+                row = Integer.parseInt(input);
+                if(row>=2 && row<=10)
+                    again = false;
+                else{
+                    input = JOptionPane.showInputDialog("Write number 2 to 10 !");
+                    again = true;
+                }
+                    
+            }
+            catch(Exception exc){
+                input = JOptionPane.showInputDialog("Write number 2 to 10 !");
+                again = true;
+            }
+        }
+        SpelCentral sc = new SpelCentral(row);
+    } 
     public static void main(String[] args) {
-        BildSpel bs = new BildSpel();
-//        Uppgift3 play = new Uppgift3();
+//        BildSpel bs = new BildSpel();
+        Uppgift3 play = new Uppgift3();
     }
 }
